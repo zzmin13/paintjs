836416f7c7c413e06bfdf5e85ce196cf0672f879
zzmin13 <zzzz1997@naver.com>
2021-01-29 13:02:29 +0900

    Save Image

--- app.js
+++ app.js
@@ -3,12 +3,16 @@ const ctx = canvas.getContext("2d");
 const colors = document.getElementsByClassName("jsColor");
 const range = document.getElementById("jsRange");
 const mode = document.getElementById("jsMode");
+const saveBtn = document.getElementById("jsSave");
 
 const INITIAL_COLOR = "#616161";
 const CANVAS_SIZE = 700;
 
 canvas.width = CANVAS_SIZE;
 canvas.height = CANVAS_SIZE;
+
+ctx.fillStyle = "white";
+ctx.fillRect(0,0,canvas.width, canvas.height); //캔버스 처음의 배경 색 지정, 이걸 안해주면 사진 저장했을 때 배경이 투명하게 저장이 된다.
 
 ctx.strokeStyle = INITIAL_COLOR; //처음 사용하는 사람이 이 색상으로 사용하도록 한다.
 ctx.fillStyle = INITIAL_COLOR;
@@ -64,6 +68,18 @@ function handleCanvasClick(){
     
 }
 
+function handleRightClick(event){
+    event.preventDefault();
+}
+
+function handleSaveClick(){
+    const image = canvas.toDataURL("image/png");
+    const link = document.createElement("a");
+    link.href = image;
+    link.download = "paintJS";
+    link.click();
+}
+
 if(canvas){
     //canvas가 존재하는지 확인
     canvas.addEventListener("mousemove",onMouseMove);
@@ -71,6 +87,7 @@ if(canvas){
     canvas.addEventListener("mouseup",stopPainting);
     canvas.addEventListener("mouseleave",stopPainting);
     canvas.addEventListener("click",handleCanvasClick);
+    canvas.addEventListener("contextmenu", handleRightClick);
 }
 
 Array.from(colors).forEach(color => color.addEventListener("click", handleColorClick));
@@ -81,4 +98,8 @@ if(range){
 
 if(mode){
     mode.addEventListener("click",handleModeClick);
+}
+
+if(saveBtn){
+    saveBtn.addEventListener("click",handleSaveClick);
 }
